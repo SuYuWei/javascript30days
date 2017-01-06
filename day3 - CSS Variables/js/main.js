eddec4cf043f15801d04632414c9d1c27fb8957e
Joseph.Su <a607131731@gmail.com>
2017-01-06 14:29:14 +0800

    day3 CSS Variables

--- day3 - CSS Variables/js/main.js
+++ day3 - CSS Variables/js/main.js
@@ -1,11 +1,8 @@
 var inputs = document.querySelectorAll('#controls input');
 
-
 function handleUpdate(){
 	var suffix = this.dataset.sizing || '';
 	document.documentElement.style.setProperty("--"+this.name, this.value + suffix);
-	this.classList.add("move");
-	
 }
 
 inputs.forEach(function(input){
