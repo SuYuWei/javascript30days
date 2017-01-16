b67e32cd465dd053d6109b30f21f1a107f2bf58a
Joseph.Su <a607131731@gmail.com>
2017-01-16 19:08:45 +0800

    day13 - Slide In on Scroll

--- day13 - Slide In on Scroll/js/main.js
+++ day13 - Slide In on Scroll/js/main.js
@@ -9,7 +9,7 @@ window.addEventListener('scroll', debounce(checkSlide));
 // be triggered. The function will be called after it stops being called for
 // N milliseconds. If `immediate` is passed, trigger the function on the
 // leading edge, instead of the trailing.
-function debounce(func, wait = 40, immediate = true) {
+function debounce(func, wait = 10, immediate = true) {
 	var timeout;
 	return function() {
 		var context = this, 
